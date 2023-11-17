8b77d43239833481d7eb6b53e951c86e34ffac7e
afshinrahmati <afshinrahmati93@mail.com>
2023-11-17 23:05:26 +0330

    feat: :zap: #20
    work with input

---
+++ PYTHON/input.py
@@ -0,0 +1,5 @@
+name  = input("please entair your name?")
+city = input("whic city?")
+name_pet = input("what is your name pet?\n")
+
+print("wwwoww is it greate name: " + name + "your city is" + city + "your pet name is" + name_pet)
