9eee1efc876e7a72642405b8fe02bbccbee95ff6
afshinrahmati <afshinrahmati93@gmail.com>
2023-12-18 06:41:05 +0330

    feat:call api

--- PYTHON/JADI/call-api.py
+++ PYTHON/JADI/call-api.py
@@ -1,3 +1,10 @@
 import requests
-responce = requests.get("https://api.coinbase.com/v2/prices/buy?currency=usd")
+proxies = {
+    'https':''
+}
+#https://api.coinbase.com/v2/prices/buy?currency=usd
+responce = requests.get("https://randomfox.ca/floof/")
+
+if responce.price <1000:
+    print("find it is your work")
 print(responce.text)
