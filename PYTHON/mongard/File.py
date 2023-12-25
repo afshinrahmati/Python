9379c0dd207abb2ca65e26679f76e95dbc25f483
afshin <afshinrahmati93@gmail.com>
2023-12-25 19:41:07 +0330

    feat: :art: scope and file
    scope and file
    
    scope and file

---
+++ PYTHON/mongard/File.py
@@ -0,0 +1,4 @@
+with open('mongard/a.txt', 'r') as file:
+    # Read the entire file contents
+    file_contents = file.read()
+    print(file_contents)
---
+++ PYTHON/mongard/scope.py
@@ -0,0 +1,4 @@
+a= 90 #gloabl scope
+
+def t():
+    a=89 #local scope
